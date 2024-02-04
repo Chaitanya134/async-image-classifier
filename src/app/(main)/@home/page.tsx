d3f87fa0c81140541d4a04b7ae5dd3d7d804ec67
Chaitanya Giri <chaitanyagiri2@gmail.com>
2024-02-04 22:49:08 +0530

    create upload image card

--- src/app/(main)/@home/page.tsx
+++ src/app/(main)/@home/page.tsx
@@ -1,3 +1,76 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsLoading(true);
+    const files = event.target.files;
+    if (!files || files.length === 0) return;
+    setSelectedFile(files[0]);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreviewImage(reader.result as string);
+    };
+    reader.readAsDataURL(files[0]);
+    setIsLoading(false);
+  };
+
+  const classifyDisabled = !selectedFile || isLoading;
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Upload an Image</CardTitle>
+          <CardDescription>Submit an Image for Classification</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Input
+              id="picture"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+            {isLoading && (
+              <ReloadIcon className="mx-auto mt-6 h-6 w-6 animate-spin text-slate-500" />
+            )}
+            {previewImage && selectedFile && (
+              <div className="relative aspect-video w-full">
+                <Image
+                  src={previewImage}
+                  alt={selectedFile.name}
+                  className="mt-2 object-contain text-center text-gray-600"
+                  fill
+                />
+              </div>
+            )}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="ml-auto" disabled={classifyDisabled}>
+            Classify
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
