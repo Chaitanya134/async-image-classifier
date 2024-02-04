e6bc630acd2702092fb7247227e61a80350628e9
Chaitanya Giri <chaitanyagiri2@gmail.com>
2024-02-05 04:25:26 +0530

    show completed images

--- src/app/(main)/@completed/page.tsx
+++ src/app/(main)/@completed/page.tsx
@@ -1,3 +1,51 @@
-export default function CompletedPage() {
-  return <div>Completed</div>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getJobsInCompletedState } from "@/data-access/job";
+import Image from "next/image";
+import { formatDistanceToNow } from "date-fns";
+import { getFileExtension } from "@/lib/utils";
+
+export const dynamic = "force-dynamic";
+
+export default async function CompletedPage() {
+  const completedJobs = await getJobsInCompletedState();
+  return (
+    <div className="grid h-full place-items-center overflow-auto">
+      <div className="flex w-full flex-col items-center justify-center gap-4 py-8">
+        {completedJobs.map((job) => {
+          const isSvg = getFileExtension(job.imageName) === "svg";
+          return (
+            <Card key={job.id} className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle className="line-clamp-1" title={job.imageName}>
+                  {job.imageName}
+                </CardTitle>
+                <CardDescription title={job.createdAt.toString()}>
+                  {formatDistanceToNow(job.createdAt, { includeSeconds: true })}{" "}
+                  ago
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <div className="relative aspect-video w-full">
+                    <Image
+                      src={`data:image/${isSvg ? "svg+xml" : "png"};base64,${job.image}`}
+                      alt={"Image sent for classification"}
+                      className="mt-2 object-contain text-center text-gray-600"
+                      fill
+                    />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/data-access/job.ts
+++ src/data-access/job.ts
@@ -17,3 +17,21 @@ export async function getJobsInPendingState() {
     image: images.reduce((data, image) => data + image.data, ""),
   }));
 }
+
+export async function getJobsInCompletedState() {
+  const jobs = await prisma.imageClassificationJob.findMany({
+    include: {
+      images: true,
+    },
+    where: { status: "COMPLETED" },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  // Combine the image chunks into one single image
+  return jobs.map(({ images, ...job }) => ({
+    ...job,
+    image: images.reduce((data, image) => data + image.data, ""),
+  }));
+}
